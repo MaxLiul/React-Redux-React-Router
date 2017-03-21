bc4d0b650db7371503379a8208aa03c125b90018
MaxLiul <maksym.liul@brander.ua>
2017-03-21 19:04:54 +0200

    news

---
+++ src/components/News.jsx
@@ -0,0 +1,12 @@
+import React /* , {PropTypes, Component }*/ from 'react';
+const News = React.createClass(object2);
+function renderFunction2() {
+  return (
+    <div>
+    We have very good news
+    </div>);
+}
+const object2 = {};
+object2.render = renderFunction2;
+
+export default News;
