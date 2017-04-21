b90a3b17857ae5ce8643e0c62941ea1aec1d0a8b
MaxLiul <maksym.liul@brander.ua>
2017-04-21 15:38:29 +0300

    21.04

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ export default class App extends Component {
   static propTypes = {
     children: React.PropTypes.array.isRequired
   }
+
   render() {
     return (
       <div className='container'>
