c3416ade3b721a8e75073d42dee910b445a75104
MaxLiul <maksym.liul@brander.ua>
2017-05-29 20:23:16 +0300

    newStyle

--- myapp/models/assosiatedDb.js
+++ myapp/models/assosiatedDb.js
@@ -6,21 +6,21 @@ const config = {
   password: 'testuser',
   options: {
     host: 'localhost',
-     port: 5432,
-     timezone: 'GMT-0',
-     dialect: 'postgres',//'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',
-     logging: false, // process.env.NODE_ENV === 'development' ||process.env.NODE_ENV === 'test',
-     pool: {
-       max: 10,
-       min: 5,
-       idle: 30000
-     }
+    port: 5432,
+    timezone: 'GMT-0',
+    dialect: 'postgres', //  'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql',
+    logging: false, // process.env.NODE_ENV === 'development' ||process.env.NODE_ENV === 'test',
+    pool: {
+      max: 10,
+      min: 5,
+      idle: 30000
+    }
   }
-}
+};
 
 const db = {};
 
-var sequelize = new Sequelize(config.database, config.username, config.password, config.options);
+const sequelize = new Sequelize(config.database, config.username, config.password, config.options);
 
 sequelize
   .authenticate()
@@ -46,40 +46,41 @@ db.Job = sequelize.define('job', {
 });
 db.User.belongsTo(db.Job);
 
-  // force: true will drop the table if it already exists
-db.User.sync({ force: true })
-.then(() => {
-  //  Table created;
-  return db.User.bulkCreate([ {
-    firstName: 'Max',
-    lastName: 'Liul'
-  },
-  {
-    firstName: 'Maxim',
-    lastName: 'Liul'
-  },
-  {
-    firstName: 'Maks',
-    lastName: 'Liul'
-  } ]);
+db.Job.sync({ force: true }).
+then(() => {
+  return db.Job.create({
+    possition: 'Programmer1'
+  });
 }).
-  then(() => {
-    return  db.User.findOrCreate({ where: {
-      firstName: 'Nikolo',  lastName: 'Tesla'
-    } });
+then(() => {
+  return db.Job.create({
+    possition: 'Programmer2'
   });
-
-db.Job.sync({ force: true })
-  .then(() => {
-    //  Table created;
-    return db.Job.bulkCreate([ {
-      possition: 'Programmer'
-    },
-    {
-      possition: 'Programmer'
-    } ]);
+}).
+then(() => {
+  return db.Job.findOrCreate({ where: {
+    possition: 'Programmer3 '
+  } });
+}).
+then(() => {
+  //  Table created;
+  return db.User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
   });
-
+}).
+then(() => {
+  return db.User.findOrCreate({ where: {
+    firstName: 'Alex',
+    lastName: 'Pushkin'
+  } });
+}).
+then(() => {
+  return db.User.findOrCreate({ where: {
+    firstName: 'Sidor',
+    lastName: 'Ivanov'
+  } });
+});
 
 module.exports = db;
 
